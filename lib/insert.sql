3e942906cc6cbbbc4ec3ba2a51c6ca1fd4c63c48
jenna-lab <jennifer.sammy@student.moringaschool.com>
2023-02-16 21:57:04 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age , sex ,color, temperament, alive)
+VALUES ('Mr.Chocolate', 55, 'M', 'White', 'Sanguin', 0 ),
+       ('Rowdy', 45, 'M', 'White', 'Sanguin', 0 ),
+       ('Tabitha', 35, 'M', 'White', 'Sanguin', 0 ),
+       ('Sergeant Brown', 65, 'F', 'White', 'Sanguin', 0 ),
+       ('Melissa', 25, 'M', 'White', 'Sanguin', 0 ),
+       ('Grinch', 15, 'F', 'White', 'Sanguin', 0 ),
+       ('Wendy', 10, 'M', 'White', 'Sanguin', 0 ),
+       (NULL, 5, 'F', 'White', 'Sanguin', 1 );
